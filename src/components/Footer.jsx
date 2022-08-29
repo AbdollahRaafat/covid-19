3b72b8e1d0b46274bc47a3c7153b14934b816d0a
abdollahraafat <abdollah.raafat.daif@gmail.com>
2022-08-29 17:47:44 +0200

    Add background-image on the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Footer = () => (
-  <div className="py-8 flex justify-center items-center">
+  <div className="py-8 flex justify-center items-center hero">
     <h1 className="font-thin text-gray-100 tracking-wide">
       &copy; Classic 2022. All rights reserved. Developed by
       {' '}
-      <a className="text-white font-bold" href="https://github.com/AbdollahRaafat">Abdollah Raafat.</a>
+      <a className="text-white font-bold"  href="https://github.com/AbdollahRaafat">Abdollah Raafat.</a>
       {' '}
     </h1>
   </div>
