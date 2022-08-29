7ad8f16a94faf56dc32c8f0855d39822c118614a
abdollahraafat <abdollah.raafat.daif@gmail.com>
2022-08-29 17:49:04 +0200

    Display footer sticky to bottom

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Footer = () => (
-  <div className="py-8 flex justify-center items-center hero">
+  <div className="py-5 flex justify-center items-center hero bottom-0 w-full sticky">
     <h1 className="font-thin text-gray-100 tracking-wide">
       &copy; Classic 2022. All rights reserved. Developed by
       {' '}
-      <a className="text-white font-bold"  href="https://github.com/AbdollahRaafat">Abdollah Raafat.</a>
+      <a className="text-white font-bold" href="https://github.com/AbdollahRaafat">Abdollah Raafat.</a>
       {' '}
     </h1>
   </div>
