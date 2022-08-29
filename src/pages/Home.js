5e43e2d56b86d9ff62f254815d11bce16dd458a2
abdollahraafat <abdollah.raafat.daif@gmail.com>
2022-08-29 17:43:59 +0200

    Display country cards in grid

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchCovid } from '../store/covid/covid';
+import Country from '../components/Country';
 
-const Home = () => (
-  <div>
-    <h1>Home Page</h1>
-  </div>
-);
+const Home = () => {
+  const covidData = useSelector((state) => state.covidReducer.covidData);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCovid());
+  }, []);
+
+  return (
+    <div>
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
+        {covidData.map((country) => (
+          <Country
+            key={country.ID}
+            country={country.Country}
+            TotalConfirmed={country.TotalConfirmed}
+          />
+        ))}
+      </div>
+      <h1>Home Page</h1>
+    </div>
+  );
+};
 export default Home;
--- src/store/covid/covid.js
+++ src/store/covid/covid.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const FETCH_COVID_DATA = 'covid/FETCH_COVID_DATA';
 
 // App initial state
@@ -6,10 +8,16 @@ const initialState = {
 };
 
 // Actions
-export const fetchCovidResults = (payload) => ({
-  type: 'FETCH_COVID_DATA',
+export const fetchCovidData = (payload) => ({
+  type: FETCH_COVID_DATA,
   payload,
 });
+
+export const fetchCovid = () => async (dispatch) => {
+  const covidData = await axios.get('https://api.covid19api.com/summary');
+  const data = await covidData.data;
+  dispatch(fetchCovidData(data.Countries));
+};
 
 // Reducer
 const reducer = (state = initialState, action) => {
